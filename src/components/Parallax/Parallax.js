1b8bbcc23ace556be7c88d81ee37b0676df45eb3
paschalidi <paschalidis@factor-e.eu>
2016-12-01 02:02:56 +0200

    master: In Parallax image and height loads dynamically

--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -1,37 +1,38 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Parallax.css';
+import img from '../../public/assets/background_lamps.jpg';
 
 class Parallax extends React.Component {
   static propTypes = {
-    h2: PropTypes.string.isRequired,
-    h3: PropTypes.oneOf(['string', 'node']),
+    h2: PropTypes.string,
+    h3: PropTypes.string,
+    p: PropTypes.string,
     backgroundImagePath: PropTypes.string.isRequired,
     height: PropTypes.number.isRequired,
-    children: PropTypes.node.isRequired,
+    children: PropTypes.node,
   };
 
   static defaultProps = {
-    h2: 'spread the',
-    h3: <i className="fa fa-heart-o fa-3" />,
-    backgroundImagePath: PropTypes.string.isRequired,
+    h2: null,
+    h3: null,
+    p: null,
+    backgroundImagePath: PropTypes.string,
     height: '90vh',
   };
 
   render() {
-    const { h2, h3, backgroundImagePath, height, children } = this.props;
-    // TODO make background and heigh Dynamic with values backgroundImage and height.
-    const backgroundImage = require('../../public/assets/background_bridge_edited.jpg');
-    // const backgroundImage = require({backgroundImagePath});
+    const { h2, h3, p, backgroundImagePath, height, children } = this.props;
 
     return (
-      <div className={s.banner} style={{ height: '100vh' }}>
+      <div className={s.banner} style={{ height: `${height}vh` }}>
         <div className={s.clip}>
-          <div className={s.bgBgChrome} style={{ backgroundImage: 'url(' + backgroundImage + ')' }} />
+          <div className={s.bgBgChrome} style={{ backgroundImage: `url(${img})` }} />
         </div>
         <div className={s.mainTitle}>
           <h2>{h2}</h2>
           <h3>{h3}</h3>
+          <p>{p}</p>
         </div>
         {children}
       </div>
